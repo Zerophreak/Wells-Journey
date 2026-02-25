a98b7bf620ce2a3254fd56a1f66246d3911c1945
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-02-25 21:12:08 +0100

    main scene change

---
+++ Scenes/Main/Main.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Main : Control
+{
+	public override void _UnhandledInput(InputEvent, @event)
+	{
+		if(@event.IsActionPressed("quit"))
+		{
+			GameManager.ChangeToLevel();
+		}
+	}
+}
